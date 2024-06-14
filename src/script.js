05606d2823792487bb9abeca5cc3ff5223dfb53f
alexander <alexanderzoet@gmail.com>
2024-06-14 13:41:18 +0200

    alex

--- src/script.js
+++ src/script.js
@@ -1,3 +1,39 @@
 document.getElementById('launchButton').addEventListener('click', function() {
     window.location.href = 'steam://run/322170';
 });
+
+let data;
+
+function loadinglocalstorage() {
+    fetch("games.json")
+        .then(response => response.json())
+        .then(gamedata => {
+            data = gamedata.games;
+            localStorage.setItem("data", JSON.stringify(data));
+            createcards(data);
+        });
+}
+
+function createcards(data) {
+    const section = document.getElementById("newSection");
+
+data.forEach(game => {
+    const img = document.createElement("img");
+    img.src = game.img;
+    img.alt = game.title;
+    section.appendChild(img);
+
+    const play = document.createElement('button')
+    play.textContent = 'Play'
+    play.id = 'launchButton'
+    section.appendChild(play)
+
+    const info = document.createElement('button')
+    info.textContent = 'infoButton'
+    info.id = 'infobutton'
+    section.appendChild(info)
+});
+}
+
+console.log(localStorage);
+loadinglocalstorage();
