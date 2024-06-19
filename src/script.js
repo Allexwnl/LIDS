1186a3afbe1d5d0b0956abf3274fc78eeee5de1a
Anselm Wilmont <wil.anselm@gmail.com>
2024-06-19 12:05:31 +0200

    Anselm

--- src/script.js
+++ src/script.js
@@ -1,39 +1,39 @@
-document.getElementById('launchButton').addEventListener('click', function() {
-    window.location.href = 'steam://run/322170';
-});
+document.addEventListener('DOMContentLoaded', function() {
+    let data;
 
-let data;
+    function loadinglocalstorage() {
+        fetch("games.json")
+            .then(response => response.json())
+            .then(gamedata => {
+                data = gamedata.games; 
+                localStorage.setItem("data", JSON.stringify(data));
+                createcards(data);
+            });
+    }
 
-function loadinglocalstorage() {
-    fetch("games.json")
-        .then(response => response.json())
-        .then(gamedata => {
-            data = gamedata.games; 
-            localStorage.setItem("data", JSON.stringify(data));
-            createcards(data);
+    function createcards(data) {
+        const section = document.getElementById("newSection");
+
+        data.forEach(game => {
+            const img = document.createElement("img");
+            img.src = game.img;
+            img.alt = game.title;
+            section.appendChild(img);
+
+            const play = document.createElement('button');
+            play.textContent = 'Play';
+            play.addEventListener('click', function() {
+                window.location.href = `steam://run/${game.appId}`;
+            });
+            section.appendChild(play);
+
+            const info = document.createElement('button');
+            info.textContent = 'Info';
+            info.id = 'infobutton';
+            section.appendChild(info);
         });
-}
+    }
 
-function createcards(data) {
-    const section = document.getElementById("newSection");
-
-data.forEach(game => {
-    const img = document.createElement("img");
-    img.src = game.img;
-    img.alt = game.title;
-    section.appendChild(img);
-
-    const play = document.createElement('button')
-    play.textContent = 'Play'
-    play.id = 'launchButton'
-    section.appendChild(play)
-
-    const info = document.createElement('button')
-    info.textContent = 'infoButton'
-    info.id = 'infobutton'
-    section.appendChild(info)
+    console.log(localStorage);
+    loadinglocalstorage();
 });
-}
-
-console.log(localStorage);
-loadinglocalstorage();
